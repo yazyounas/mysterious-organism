fea32a707cf2382bc5679e013b65f18a3c45edb9
Yasir Younas <yazyounas@gmail.com>
2022-10-12 15:23:48 +0100

    added function with object properties that return dna property

--- mystOrg.js
+++ mystOrg.js
@@ -12,4 +12,31 @@ const returnRandBase = () => {
     }
     return newStrand
   }
-  
+  // Returns a random single strand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = []
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase())
+  }
+  return newStrand
+}
+
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum,
+    dna,
+    mutate() {
+      
+    let randomIndex = Math.floor(Math.random() * this.dna.length)
+    let newBase = returnRandBase();
+    console.log(randomIndex)
+    console.log(newBase)
+    while (this.dna[randomIndex] === newBase) {
+      newBase = returnRandBase();
+    }
+    this.dna[randomIndex] = newBase
+    return this.dna
+  }
+  }
+};
+console.log(pAequorFactory(2, ['A', 'T', 'C', 'G']).mutate())
